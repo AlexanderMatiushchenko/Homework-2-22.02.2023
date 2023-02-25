262466580cb3dc0e09ba94fea0e6ed275c61ce63
AlexanderMatiushchenko <krasty1313@gmail.com>
2023-02-25 16:46:50 +0100

    Initial commit

---
+++ src/com/alex/java/homework2/main.java
@@ -0,0 +1,8 @@
+package com.alex.java.homework2;
+
+public class main {
+    public static void main(String[]args){
+        System.out.println("Hello I’m main from homework2");
+    }
+}
+
---
+++ src/de/telran/project/homework2/main.java
@@ -0,0 +1,7 @@
+package de.telran.project.homework2;
+
+public class main {
+    public static void main(String[]args){
+        System.out.println("Hello I’m main from homework2");
+    }
+}
